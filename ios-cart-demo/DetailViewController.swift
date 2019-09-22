d836e7e5967079378ea3bb69ee91c72ec4721f2f
Kelvin Fok <punkd1@gmail.com>
2019-09-22 11:31:59 +0800

    add detail view controller and pass user profiles model

---
+++ ios-cart-demo/DetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailViewController.swift
+//  ios-cart-demo
+//
+//  Created by Kelvin Fok on 22/9/19.
+//  Copyright © 2019 Kelvin Fok. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var userProfile: UserProfile!
+    
+    @IBOutlet weak var favouriteColorView: UIView!
+    @IBOutlet weak var hobbiesLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameLabel.text = userProfile.name
+        hobbiesLabel.text = userProfile.hobby
+        ageLabel.text = String(describing: userProfile.age)
+        favouriteColorView.backgroundColor = .brown
+    }
+}
--- ios-cart-demo/ViewController.swift
+++ ios-cart-demo/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
+struct UserProfile {
+    let name: String
+    let age: Int
+    let hobby: String
+    let favouriteColor: UIColor
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var hobbyTextField: UITextField!
+    @IBOutlet weak var colorSegmentedControl: UISegmentedControl!
+    
+    private let showDetailsSegue = "showDetails"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .groupTableViewBackground
     }
 
-
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        
+        var favouriteColor: UIColor
+        
+        let name = nameTextField.text ?? ""
+        let age = Int(ageTextField.text!) ?? 0
+        let hobby = hobbyTextField.text ?? ""
+        let index = colorSegmentedControl.selectedSegmentIndex
+        
+        switch index {
+        case 0:
+            favouriteColor = .blue
+        default:
+            favouriteColor = .yellow
+        }
+        
+        let userProfile = UserProfile(name: name, age: age, hobby: hobby, favouriteColor: favouriteColor)
+        
+        performSegue(withIdentifier: showDetailsSegue, sender: userProfile)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showDetailsSegue, let userProfile = sender as? UserProfile {
+            let destination = segue.destination as! DetailViewController
+            destination.userProfile = userProfile
+        }
+    }
+    
 }
 
